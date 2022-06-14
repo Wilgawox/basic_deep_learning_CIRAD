36aa27b2336300ea7f8292229b0aa1122417f883
Thibault <inferno0980@gmail.com>
2022-06-14 11:07:47 +0200

    Pre_data commit

--- dataset_config.py
+++ dataset_config.py
@@ -1,5 +1,7 @@
+#Imports
+from audioop import error
 from glob import glob
-
+import pandas as pd
 import IPython.display as display
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,64 +13,82 @@ from tensorflow.keras.optimizers import Adam
 from IPython.display import clear_output
 import tensorflow_addons as tfa
 from PIL import Image, ImageSequence
-
 import modif_image
 import data_prep_3D
+from skimage.io import imread, imshow
+from skimage.transform import resize
 
-image_path = "Data_Thibault/"
-dataset_path = "Data_Thibault/data"
-training_data_path = "data_training"
-training_data = "training/input/"
-val_data_path = "data_validation"
-val_data = "training/results/"
+# Data paths
+training_data = "Data_Thibault/input/"
+res_data = "Data_Thibault/results/"
+dataset_path = "Data_Thibault/data/"
+training_data_path = dataset_path+"data_training/"
+val_data_path = dataset_path+"data_validation/"
+test_data_path = dataset_path+"data_test"
 
+# Parameters for the tiles :
+SIZE = [512,512] #[x,y] dimensions of a tile
+IMG_H = SIZE[0]
+IMG_W = SIZE[1]
+STRIDE =  450 #Stride between two tiles in a image
+INT_ROOT = -1 #Value of a root pixel
+INT_BG = 1 #Value of a backgroung pixel
 
+#Table de permutation -> Classification
+#18/9/9
 
+list_X = []
+for filename in glob(training_data+'*.tif'):
+    im=Image.open(filename)
+    list_X.append(data_prep_3D.create_Xarray(im))
 
-#IMG_SIZE = Not for now
-N_CHANNELS = 1
-N_CLASSES = 2 #softmax
-#BATCH_SIZE = ???
-#BUFFER_SIZE = ???
-
-#train_set = tf.data.Dataset.list_files(dataset_path + training_data + "*.tif")
-#TRAINSET_SIZE = len(glob(dataset_path + val_data + "*.tif"))
-#print(f"The Training Dataset contains {TRAINSET_SIZE} images.")
-
-#valid_set = tf.data.Dataset.list_files(dataset_path + training_data + "*.tif")
-#VALSET_SIZE = len(glob(dataset_path + val_data + "*.tif"))
-#print(f"The Validation Dataset contains {VALSET_SIZE} images.")
-
-#AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-#'''
-#gpus = tf.config.experimental.list_physical_devices('GPU')
-#if gpus:
-#    try:
-#        for gpu in gpus:
-#            tf.config.experimental.set_memory_growth(gpu, True)
-#        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-#        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-#    except RuntimeError as e:
-#        print(e)'''
+list_Y = []
+for filename in glob(res_data+'*.tif'):
+    im=Image.open(filename)
+    list_Y.append(data_prep_3D.create_Yarray(im))
 
 
-#'''----------------------------------------------------------------
-#SETUP OF DATASETS
-#-----------------------------------------------------------------'''
+def image_writing(list_X, list_Y) : 
+   for i in range(1,2):#in range(list(list_X)) :
+        for j in range(len(list_X[i])):
+            tilesX = modif_image.img_processing(list_X[i][j], -1, 1, [512,512], 450)
+            tilesY = modif_image.img_processing(list_Y[i][j], -1, 1, [512,512], 450)
+            if len(tilesX)!=len(tilesY) :
+                raise Exception('Problem while cutting tiles', 'Different number of tiles')
+            if i<=len(list_X)//2 :
+                #50% of images in training
+                np.save((training_data_path+'training/ML1_input_'+str(i)+'.'+str(j)), tilesX)
+                np.save((training_data_path+'training/ML1_result_'+str(i)+'.'+str(j)), tilesY)
+            elif i>=3*len(list_X)//4 :
+                #25% of images in test
+                np.save((test_data_path+'test/ML1_input_'+str(i)+'.'+str(j)), tilesX)
+                np.save((test_data_path+'test/ML1_result_'+str(i)+'.'+str(j)), tilesY)
+            else :
+            #25% of images in validation
+                np.save((val_data_path+'val/ML1_input_'+str(i)+'.'+str(j)), tilesX)
+                np.save((val_data_path+'val/ML1_result_'+str(i)+'.'+str(j)), tilesY)
 
-#dataset = {"train": train_set, "val": valid_set}
+image_writing(list_X, list_Y);
 
-#dataset['train'] = dataset['train'].map(data_prep_3D.create_Xarray, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-#dataset['train'] = dataset['train'].shuffle(buffer_size=BUFFER_SIZE)
-#dataset['train'] = dataset['train'].repeat()
-#dataset['train'] = dataset['train'].batch(BATCH_SIZE)
-#dataset['train'] = dataset['train'].prefetch(buffer_size=AUTOTUNE)
+N_CHANNELS = 1 
+N_CLASSES = 2 #softmax #Root or background
+NUM_TEST_IMAGES = 0 #Calcul a faire avant execution
 
-#dataset['val'] = dataset['val'].map(data_prep_3D.create_Yarray)
-#dataset['val'] = dataset['val'].repeat()
-#dataset['val'] = dataset['val'].batch(BATCH_SIZE)
-#dataset['val'] = dataset['val'].prefetch(buffer_size=AUTOTUNE)
+#img_list = os.listdir('../Data_Thibault/data/data_test')
+#mask_list = os.listdir('../input/bbbc005_v1_ground_truth/BBBC005_v1_ground_truth')
 
-#print(dataset['train'])
-#print(dataset['val'])
+#Creation of a dataset
+df_images = pd.DataFrame(img_list, columns=['image_id'])
+
+def get_num_cells(x):
+    # split on the _
+    a = x.split('_')
+    # choose the third item
+    b = a[2] # e.g. C53
+    # choose second item onwards and convert to int
+    num_cells = int(b[1:])
+    
+    return num_cells
+
+# create a new column called 'num_cells'
+#df_images['num_cells'] = df_images['image_id'].apply(get_num_cells)
--- modif_image.py
+++ modif_image.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def img_processing(img, intensity_bg, intensity_root) :
+def reduced_centered_range(img, intensity_bg, intensity_root) :
     #Process the img to adjust the value between the 2 intensities, and reverse the black and white
     #Prendre les points de racines -> Fait une mediane ->pareil pour bgd
     maxx = np.amax(img)
@@ -9,11 +9,12 @@ def img_processing(img, intensity_bg, intensity_root) :
     a = (intensity_bg-intensity_root)/(maxx-minn)
     b = intensity_bg-a*maxx
     imgCR = a*img+b
-    return imgCR, a, b
+    return np.array(imgCR), a, b
 
 
 
 def img_split_no_borders(img : np.array, final_size : tuple, stride) :
+    #DEPRECATED
     #Slice img to tiles in final_size shape, and cut the bits that do not fit
     img_w, img_h = img.shape
     tile_w, tile_h = final_size
@@ -28,6 +29,7 @@ def img_split_no_borders(img : np.array, final_size : tuple, stride) :
 
 def img_split_remnants(img, tile_size : tuple, stride) :
     #Try to slice img to tiles in final_size shape, and slide the rest to be of the same size
+    img = np.array(img)
     img_w, img_h = img.shape
     tile_w, tile_h = tile_size
     tiles=[]
@@ -44,3 +46,7 @@ def img_split_remnants(img, tile_size : tuple, stride) :
 
 #Faire fonction qui prends pas le blocs sans racines ou qui pondere le %tage de bloc sans racines
 
+def img_processing(img : np.array, intensity_bg, intensity_root, tile_size : tuple, stride : int) : 
+        img = reduced_centered_range(img, intensity_bg, intensity_root)
+        tiles = img_split_remnants(img, tile_size, stride)
+        return tiles
